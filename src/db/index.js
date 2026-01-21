b5d72180800bc5e9d18c6dcf426d791c5a7b6bf3
Akshay <sharma.akshay2782629@gmail.com>
2026-01-21 22:53:04 +0530

    Mongodb error

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 import { DB_Name } from "../constants.js";
+import dotenv from "dotenv";
 
-const connectDB = async() =>{
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`)
-        console.log(`MONGODB Connected successfully! DB Host: ${connectionInstance.connection.host}`);
-        console.log(`Database Name: ${DB_Name}`);
-        
-    } catch (error) {
-        console.log("ERROR : " , error);
-        process.exit(1)
-        
-    }
-}
-export default connectDB
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_Name}`
+    );
+    console.log(
+      `MONGODB Connected successfully! DB Host: ${connectionInstance.connection.host}`
+    );
+    console.log(`Database Name: ${DB_Name}`);
+  } catch (error) {
+    console.log("ERROR : ", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
